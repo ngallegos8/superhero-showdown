1a317bfe9d17878655aa71573755c11fa925ea59
Chris <conley.christopher0@gmail.com>
2024-01-02 12:26:21 -0700

    changes to html and index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import { apiKey } from './key.js';
+import { apiKey } from "./key.js";
 
+function handleAPIQuery(event) {
+    event.preventDefault();
+    const searchTerm = event.target.query.value;
+    console.log(searchTerm);
+    searchAPI(searchTerm);
 
-fetch(`https://www.superheroapi.com/api.php/${apiKey}/search/a`)
+    fetch(`https://www.superheroapi.com/api.php/${apiKey}/search/${searchTerm}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
         })
         .catch(error => console.log(error));
+}
+
+console.log(handleAPIQuery(superman));
